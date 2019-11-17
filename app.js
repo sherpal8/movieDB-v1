37542ff3beb951a56e853308c2e61a550278c649
sherpal8 <sherpalkorotana@icloud.com>
2019-11-17 18:13:23 +0000

    set up of repository files for data query i.e. movieRepo and personRepo

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 "use strict";
 
 const db = require("./db");
-const screen = require("./screen");
+const { clearTerminalScreen, write } = require("./screen");
+
+clearTerminalScreen();
+
+db("movie")
+  .count()
+  .then(function(result) {
+    write(result, "pretty");
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+  .finally(function() {
+    db.destroy();
+  });
---
+++ personRepo.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const Promise = require("bluebird"); // 3rd party Promise library
+const db = require("./db");
+
+module.exports = {};
